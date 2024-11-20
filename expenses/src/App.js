ac22547259ddb253e43b955cbb8aa13a030895f0
Jakob Kaur <Jakob-kaur.kelder@voco.ee>
2024-11-20 11:26:44 +0200

    fixed

--- expenses/src/App.js
+++ expenses/src/App.js
@@ -1,24 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
+import Expenses from './components/Expenses/Expenses';
 
 function App() {
+  const expenses = [
+    {
+      date: new Date(2024,11,11),
+      title: 'New book',
+      price: 30.99
+    },
+    {
+      date: new Date(2024,11,11),
+      title: 'New JEANS B)',
+      price: 99.99
+    },
+  ]
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Expenses data={expenses}/>
   );
 }
 
